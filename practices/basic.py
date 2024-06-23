36a479a0df96938f7d52a49a09a8cda98803db54
Nguyễn Thế An <nguyenthean12062002@gmail.com>
2024-06-23 23:43:22 +0700

    backup documents and lesson

---
+++ practices/basic.py
@@ -0,0 +1,13 @@
+"""
+các kiểu dữ liệu trong python 
+str, number(int, float), bool(true,false), 
+các phép toán tử cơ bản
+=, ==, >=,<=, is, isnot, >, < !=
+"""
+
+a = 20
+b = 22
+# chu vi hình chữ nhật
+chuvi = (a + b) / 2
+dientich = a * b
+print(chuvi, dientich)
---
+++ practices/ham_toan_hoc.py
@@ -0,0 +1,30 @@
+"""
+import math để sử dụng các hàm toán học trong python
+help(math) => để xem thêm các hàm tính toán trong python
+"""
+from math import *
+# hàm căn bậc hai
+s = sqrt(66)
+# hàm làm tròn kết quả khi căn bậc hai
+isqrt()
+
+# hàm tính lũy thùy
+pow(2,10)
+
+# hàm làm tròn lên ( làm tròn lên số nguyên gần nhất lớn hơn tham số được truyền vào)
+ceil() 
+
+# hàm làm tròn xuống
+floor()
+
+# hàm  dai thừa
+factorial(3)
+
+# hàm lấy giá trị tuyệt đối
+fabs()
+
+# tìm giá trị nhỏ nhất, lớn nhất
+min(), max()
+
+# hàm tính tổng
+sum()
